6718712589334eaea89db1c0d31f50b6e272eb8f
Ethan Jolles <erjolles@gmail.com>
2024-08-19 15:59:25 -0400

    Init

---
+++ src/test/java/com/cmsc495/ticketsystem/TicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmsc495.ticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
