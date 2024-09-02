563300748f845eedd7a946ab9cf51b1d283a8a98
castonjv <160691852+castonjv@users.noreply.github.com>
2024-09-02 09:19:02 -0400

    Update SubmitTicketController.java
    connecting email to controller

--- src/main/java/com/cmsc495/ticketsystem/controller/SubmitTicketController.java
+++ src/main/java/com/cmsc495/ticketsystem/controller/SubmitTicketController.java
@@ -1,6 +1,8 @@
 package com.cmsc495.ticketsystem.controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,32 @@ public class SubmitTicketController {
     @GetMapping("/")
     public String showSubmitTicketPage() {
         return "submit";
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @PostMapping("/submit")
+    public String submitTicket(TicketModel ticket) {
+        // Save the ticket to the database
+        ticketRepository.save(ticket);
+
+        // Send a confirmation email to the user
+        try {
+            emailService.sendEmail(ticket.getUserEmail(), 
+                                   "Ticket Submitted", 
+                                   "Your ticket has been successfully submitted.");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            // Handle the email sending error, perhaps show an error message to the user
+        }
+
+        // Redirect or return a view
+        return "redirect:/tickets/confirmation";
+    }
+}
+
     }
 }
