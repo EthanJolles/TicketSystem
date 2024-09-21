cf6505d125bdf8257d72f57242fb0963972fa1c6
Ethan Jolles <erjolles@gmail.com>
2024-09-21 13:34:11 -0400

    Fixing test

--- src/test/java/com/cmsc495/ticketsystem/TicketSystemApplicationTests.java
+++ src/test/java/com/cmsc495/ticketsystem/TicketSystemApplicationTests.java
@@ -1,27 +1,7 @@
 package com.cmsc495.ticketsystem;
 
 import com.cmsc495.ticketsystem.config.BaseTestConfig;
-import com.cmsc495.ticketsystem.model.TicketModel;
-import com.cmsc495.ticketsystem.repository.TicketRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.assertj.core.api.Assertions.assertThat;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class TicketSystemApplicationTests extends BaseTestConfig {
 
